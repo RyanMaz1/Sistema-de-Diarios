7febe3b1f86fb5b5889b75d5537a2f28757b08d8
RyanMaz1 <129517784+RyanMaz1@users.noreply.github.com>
2023-08-22 15:53:02 -0300

    Arquivos iniciais

---
+++ JavaScript/Busca.js
@@ -0,0 +1,9 @@
+const searchInput = document.getElementById("search-input");
+const suggestionsContainer = document.getElementById("suggestions-container");
+
+searchInput.addEventListener("input", () => {
+    const searchTerm = searchInput.value;
+});
+
+searchInput.addEventListener("input", () => {
+});
